d55538cba634e7196cfd7b02ed6c9a71cb7e81a0
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-09-13 20:00:03 -0500

    just messing around with dom selector stuff, I know for sure I need more practice with the DOM

---
+++ 01-Intro/script.js
@@ -0,0 +1,38 @@
+// document.getElementById
+const title = document.getElementById('main-title')
+console.log(title)
+
+const listItems = document.getElementsByClassName('list-item')
+console.log(...listItems)
+
+let newArr = [...listItems]
+console.log(newArr)
+
+newArr.forEach(element => {
+    {
+        {
+            {
+                { 
+                    { 
+                        { 
+    //  element.style.list-style = 'disc'
+    element.style.color = 'lime';
+    // element.style.display = 'flex'
+    // element.style.justifyContent = 'center'
+    element.style.listStyle = 'armenian'
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // element.style.list-style = 'none'
+});
+// document.getElementsByClassName
+
+// document.getElementsByTagName
+
+// document.querySelector
+
+// document.querySelectorAll
